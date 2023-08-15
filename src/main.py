d7ec70db1c8d0fd392a14c769c9bf07feb3f2c29
Cooper <Cooperransom08@gmail.com>
2023-08-15 16:58:09 -0400

    add face&eye detection still \need to implement 'f15' key presses

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ class globals:
     '1080p': (1920, 1080),
     '4k': (3840, 2160),
   }
-  frame_rate = 10.0
+  frame_rate = 20.0
   resolution = (1280, 720)
   MODE = 'FACE-CAPTURE'  # Valid modes: 'FACE-CAPTURE' / 'MOTION-CAPTURE'
   last_message_sent = os.popen('time /t').read().replace('\n', '')  # Changes during runtime
@@ -71,7 +71,7 @@ class LocalHelper:
           Fout.write(content)
       else:
         target_files.remove(target)
-      
+
     zip_file = shutil.make_archive(dump_dir, 'zip', dump_dir)
     shutil.rmtree(dump_dir)
     for target in target_files:
@@ -82,7 +82,7 @@ class LocalHelper:
 # TODO's
 '''
 * OPTIMIZE
-* Implement eye detection in test.py
+* Implement eye detection in test.py ✔️
 * Make it send a 'F15' input so we dont go to sleep
 * Add a movement detector maybe??
 * Implement threading to help with consistency and pauses
@@ -162,4 +162,92 @@ class faceDetection:
     cv2.destroyAllWindows()
 
 
-faceDetection()
+class faceEyeDetection:
+
+  def __init__(self) -> None:
+    video_output_name = f'video/cam #{funcs.uniqueIDGen()} {LocalHelper.GetURLSafeTime()}.avi'
+    faceCascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+    eyeCascade = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(video_output_name, fourcc, globals.frame_rate, globals.resolution)
+    cap = cv2.VideoCapture(0)
+    cap.set(3, globals.resolution[0])  # set Width
+    cap.set(4, globals.resolution[1])  # set Height
+
+    while True:
+      ret, img = cap.read()
+      img = cv2.flip(img, 1)
+      gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+      faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5,
+        minSize=(20, 20)
+      )
+
+      for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
+        eyes = eyeCascade.detectMultiScale(
+          roi_gray,
+          scaleFactor=1.4,
+          minNeighbors=10,
+          minSize=(5, 5),
+        )
+
+        for (ex, ey, ew, eh) in eyes:
+          cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
+
+      out.write(img)
+      cv2.imshow('video', img)
+
+      if str(faces) == '()' and str(eyes) == '()':
+        pass
+      else:
+        if str(faces) != '()':
+          print(f'Face found: {funcs.getTime()}')
+        if str(eyes) != '()':
+          print(f'Eye found: {funcs.getTime()}')
+        snapshot_file = f'facedump/{funcs.uniqueIDGen()}.png'
+        cv2.imwrite(snapshot_file, img)
+        LocalHelper.sendDiscordAlert(video_output_name, snapshot_file)
+
+      if len(os.listdir('facedump')) >= 50:
+
+        def dumpCheck():
+          print('OVERFLOW')
+          files = []
+          for file in os.listdir('facedump'):
+            file = f'facedump/{file}'
+            files.append(file)
+          LocalHelper.zipFiles(files, 'facedump')
+
+        thread1 = THC.Thread(dumpCheck())
+        thread1.start()
+      if len(os.listdir('video')) >= 35:
+
+        def videoCheck():
+          print('OVERFLOW')
+          files = []
+          for file in os.listdir('video'):
+            file = f'video/{file}'
+            files.append(file)
+          LocalHelper.zipFiles(files, 'video')
+
+        thread2 = THC.Thread(videoCheck())
+        thread2.start()
+
+      key = cv2.waitKey(30) & 0xff
+      if key == 27:  # press 'ESC' to quit
+        break
+      else:
+        time.sleep(0.2)
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+faceEyeDetection()

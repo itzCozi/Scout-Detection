8d60607e18d8a18adf4ba67e00d7acab3f4517dc
Cooper <Cooperransom08@gmail.com>
2023-08-14 20:06:02 -0400

    I HAVE TO GO RN THIS IS A QUICK COMMIT

--- src/main.py
+++ src/main.py
@@ -22,30 +22,14 @@ class globals:
   }
   frame_rate = 10.0
   resolution = (1280, 720)
-  last_message_sent = os.popen('time /t').read().replace('\n', '')  # Changes during runtime
+  MODE = 'FACE-CAPTURE'  # Valid modes: 'FACE-CAPTURE' / 'MOTION-CAPTURE'
+  last_message_sent = os.popen('time /t').read().replace(
+    '\n', '')  # Changes during runtime
+  webhook_url = 'https://discord.com/api/webhooks/1140680728519127092/9_a9xGQKaE59MiyY8r8SkaA6RwceDBYXlFe6eW8NqEtr0hoBJzaNyqRfJ7_SDtIkrWz1'
+  webhook = DiscordWebhook(url=webhook_url)
 
 
 class LocalHelper:
-
-  def faceDetection():
-    if str(faces) == '()':
-      print('No faces')
-    else:
-      print(f'Face found: {funcs.getTime()}')
-      snapshot_file = f'facedump/{funcs.uniqueIDGen()}.png'
-      cv2.imwrite(snapshot_file, img)
-      LocalHelper.sendDiscordAlert(video_output_name, snapshot_file)
-
-    # TODO: Implement threading here
-    if len(os.listdir('facedump')) >= 50:
-      print('OVERFLOW')
-      filesA = []
-      files = os.listdir('facedump')
-      for file in files:
-        file = f'facedump/{file}'
-        filesA.append(file)
-      LocalHelper.zipFiles(filesA)
-    # TODO: Add check for 'video' directory overflow
 
   def GetURLSafeTime():
     out = os.popen('time /t').read().replace('\n', '')
@@ -59,19 +43,19 @@ class LocalHelper:
     else:
       globals.last_message_sent = time
       embed = DiscordEmbed()
-      webhook.wait = True
+      globals.webhook.wait = True
       for file in file_path:
         file_name = file.split('/')[-1]
 
         with open(file, 'rb') as f:
           file_data = f.read()
 
-        webhook.add_file(file_data, file_name)
+        globals.webhook.add_file(file_data, file_name)
 
       embed.set_title('Scout Alert')
       embed.set_description(f'Scout detected a face: {funcs.getTime()}')
-      webhook.add_embed(embed)
-      response = webhook.api_post_request()
+      globals.webhook.add_embed(embed)
+      response = globals.webhook.api_post_request()
       print(response)
 
   def zipFiles(target_files):
@@ -104,44 +88,65 @@ class LocalHelper:
 * Output video file ✔️
 '''
 
-webhook_url = 'https://discord.com/api/webhooks/1140680728519127092/9_a9xGQKaE59MiyY8r8SkaA6RwceDBYXlFe6eW8NqEtr0hoBJzaNyqRfJ7_SDtIkrWz1'
-webhook = DiscordWebhook(url=webhook_url)
 
-video_output_name = f'video/cam #{funcs.uniqueIDGen()} {LocalHelper.GetURLSafeTime()}.avi'
-faceCascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(video_output_name, fourcc, globals.frame_rate, globals.resolution)
-cap = cv2.VideoCapture(0)
-cap.set(3, globals.resolution[0])  # set Width
-cap.set(4, globals.resolution[1])  # set Height
+class faceDetection:
 
-while True:
-  ret, img = cap.read()
-  img = cv2.flip(img, 1)
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.3,
-    minNeighbors=5,
-    minSize=(20, 20)
-  )
+  def __init__(self) -> None:
+    video_output_name = f'video/cam #{funcs.uniqueIDGen()} {LocalHelper.GetURLSafeTime()}.avi'
+    faceCascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(video_output_name, fourcc, globals.frame_rate, globals.resolution)
+    cap = cv2.VideoCapture(0)
+    cap.set(3, globals.resolution[0])  # set Width
+    cap.set(4, globals.resolution[1])  # set Height
 
-  for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    roi_gray = gray[y:y + h, x:x + w]
-    roi_color = img[y:y + h, x:x + w]
+    while True:
+      ret, img = cap.read()
+      img = cv2.flip(img, 1)
+      gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+      faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5,
+        minSize=(20, 20)
+      )
 
-  out.write(img)
-  cv2.imshow('video', img)
+      for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
 
-  LocalHelper.faceDetection()
+      out.write(img)
+      cv2.imshow('video', img)
 
-  key = cv2.waitKey(30) & 0xff
-  if key == 27:  # press 'ESC' to quit
-    break
-  else:
-    time.sleep(0.2)
+      if str(faces) == '()':
+        print('No faces')
+      else:
+        print(f'Face found: {funcs.getTime()}')
+        snapshot_file = f'facedump/{funcs.uniqueIDGen()}.png'
+        cv2.imwrite(snapshot_file, img)
+        LocalHelper.sendDiscordAlert(video_output_name, snapshot_file)
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+      # TODO: Implement threading here
+      if len(os.listdir('facedump')) >= 50:
+        print('OVERFLOW')
+        filesA = []
+        files = os.listdir('facedump')
+        for file in files:
+          file = f'facedump/{file}'
+          filesA.append(file)
+        LocalHelper.zipFiles(filesA)
+      # TODO: Add check for 'video' directory overflow
+
+      key = cv2.waitKey(30) & 0xff
+      if key == 27:  # press 'ESC' to quit
+        break
+      else:
+        time.sleep(0.2)
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+faceDetection()
